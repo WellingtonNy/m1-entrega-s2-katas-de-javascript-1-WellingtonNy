1aaab6c835b844654780f0cf1f0b5e297b672eda
Wellington <Wellingtoncpfilho@gmail.com>
2022-08-19 00:51:39 -0300

    entrega S2 Katas

--- katas1.js
+++ katas1.js
@@ -1,94 +1,123 @@
 function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+  let resposta = []
+  for (i = 1; i <= 20; i++) {
+    resposta.push(i);
+  }
+  return resposta;
 }
+console.log(oneThroughTwenty());
 
-//call function oneThroughTwenty
+
 
 function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+  let resposta = []
+  for (i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+      resposta.push(i);
+    }
+  }
+  return resposta;
 }
+console.log(evensToTwenty());
 
-//call function evensToTwenty
+
 
 function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+  let resposta = []
+  for (i = 1; i <= 20; i++) {
+    if (i % 2 === 1) {
+      resposta.push(i);
+    }
+  }
+  return resposta;
 }
+console.log(oddsToTwenty());
 
-//call function oddsToTwenty
+
 
 function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-
+  let resposta = []
+  for (i = 1; i <= 100; i++) {
+    if (i % 5 === 0) {
+      resposta.push(i);
+    }
+  }
+  return resposta;
 }
+console.log(multiplesOfFive());
 
-//call function multiplesOfFive
+
 
 function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+  let resposta = []
+  for (i = 1; i <= 100; i++) {
+    if (Number.isInteger(Math.sqrt(i))) {
+      resposta.push(i);
+    }
+  }
+  return resposta;
 }
+console.log(squareNumbers());
 
-//call function squareNumbers
+
 
 function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let resposta = []
+  for (i = 20; i >= 1; i--) {
+    resposta.push(i);
+  }
+  return resposta;
 }
+console.log(countingBackwards());
 
-//call function countingBackwards
+
 
 function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let resposta = []
+  for (i = 20; i >= 1; i--) {
+    if (i % 2 === 0) {
+      resposta.push(i);
+    }
+  }
+  return resposta;
 }
+console.log(evenNumbersBackwards());
 
-//call function evenNumbersBackwards
+
 
 function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let resposta = []
+  for (i = 20; i >= 1; i--) {
+    if (i % 2 === 1) {
+      resposta.push(i);
+    }
+  }
+  return resposta;
 }
+console.log(oddNumbersBackwards());
 
-//call function oddNumbersBackwards
+
 
 function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let resposta = []
+  for (i = 100; i >= 1; i--) {
+    if (i % 5 === 0) {
+      resposta.push(i)
+    }
+  }
+  return resposta
 }
+console.log(multiplesOfFiveBackwards());
 
-//call function multiplesOfFiveBackwards
+
 
 function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let resposta = []
+  for (i = 100; i >= 1; i--) {
+    if (Number.isInteger(Math.sqrt(i))) {
+      resposta.push(i);
+    }
+  }
+  return resposta;
 }
-
-//call function squareNumbersBackwards
+console.log(squareNumbersBackwards());
